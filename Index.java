6d57b193981d8dd6fdb3520c67563e297ac54953
MutahirAhmed <140383554+MutahirAhmed@users.noreply.github.com>
2023-07-24 13:09:50 +0500

    Add files via upload

---
+++ Index.java
@@ -0,0 +1,32 @@
+public class Index{
+    public static void main(String[] args) {
+        System.out.println("Variable & Data Type");
+        //Data Type: 1) Primitive Data Type 2) Non-Primitive
+        /*
+         * int
+         * float
+         * char
+         * double
+         * long
+         * short
+         * boolean
+         * byte
+         */
+         int a = 1;
+         System.out.println(a);
+         char b = 'o';
+         System.out.println(b);
+         boolean c = false;
+         System.out.println(c);
+         double d = 34.56;
+         System.out.println(d); 
+         float e = 23.45f;
+         System.out.println(e);                  
+    }
+} 
+
+//Naming Convention
+// Pascal Covention: ---> Class
+//Mutahir  , MutahirAhmed
+// Camel case Convention----> Function, variable 
+// age, firstName
