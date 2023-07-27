b5d5700e6fb42e98644ffc39f15648e2348eaad0
MutahirAhmed <140383554+MutahirAhmed@users.noreply.github.com>
2023-07-27 11:26:17 +0500

    Add files via upload

---
+++ ResultingDataType.java
@@ -0,0 +1,22 @@
+public class ResultingDataType {
+     public static void main(String[] args) {
+        // float a = 2;
+        // float b = 2.3f;
+        // // int b = 2;
+        // float c = a + b;
+        // System.out.println(c);
+        // byte d = 33;
+        // short f = 128;
+        // int n = d + f;
+        // System.out.println(n);
+
+        // char a = 'f';
+        // int b = 2;
+        // int c = a + b;
+        // System.out.println(c);
+
+        // char a = 'a';
+        // a++;
+        // System.out.println(a);
+     }
+}
---
+++ Strings.java
@@ -0,0 +1,13 @@
+public class Strings {
+    public static void main(String[] args) {
+        // String firstName = "Farzeen";
+        // System.out.println(firstName);
+        // String obj = new String();
+        int age =  22;
+        System.out.print("Hi");
+        System.out.println("Hi");
+        System.out.println("Hi");
+        System.out.format("Your age is %d",age);
+        System.out.printf("Your age is %d",age);
+    }
+}
