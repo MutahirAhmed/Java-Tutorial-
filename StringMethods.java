c47165e6cd2ce70f4aafb19b7b45492332b4b55f
MutahirAhmed <140383554+MutahirAhmed@users.noreply.github.com>
2023-07-31 10:39:00 +0500

    Add files via upload

---
+++ StringMethods.java
@@ -0,0 +1,40 @@
+public class StringMethods {
+    public static void main(String[] args) {
+        // String firstName = "Farzeen";
+        // System.out.println(firstName);
+        // int result;
+        // result = firstName.length();
+        // System.out.println(result);
+        // String trimmedMessage = firstName.trim();
+        // System.out.println(trimmedMessage);
+        
+        // String runString = firstName.toUpperCase();
+        // String runString = firstName.toLowerCase();
+        // System.out.println(runString);
+        // String message = "This is me";
+        // String result = message.substring(4);
+        // String result = message.substring(4,7);
+        // System.out.println(result);
+
+        // String replace = message.replace('T','F');
+        // String replace = message.replace("This","IS");
+        // System.out.println(replace);
+
+        // boolean result = message.startsWith("this");
+        // boolean result = message.endsWith("me");
+        // System.out.println(result);
+
+        // char result = message.charAt(8);
+        // System.out.println(result);
+
+        // int result = message.indexOf('s', 4);
+        // System.out.println(result);
+
+        // int result = message.lastIndexOf('s');
+        // System.out.println(result);
+        String fname = "Mutahir";
+        // boolean result = fname.equals("mutahir");
+        boolean result = fname.equalsIgnoreCase("mutahir");
+        System.out.println(result);
+    }
+}
