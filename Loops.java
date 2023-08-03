1f924483fc949e8601fa7ff8d455cd58edc53161
MutahirAhmed <140383554+MutahirAhmed@users.noreply.github.com>
2023-08-03 11:25:34 +0500

    Add files via upload

---
+++ Loops.java
@@ -0,0 +1,26 @@
+public class Loops {
+    public static void main(String[] args) {
+        // for(int i = 0; i < 10; i++)
+        // {
+        //     System.out.println("Hello " + i);
+        // }
+    //   for(int a=0; a<=5; a++){
+    // //   System.out.println(a);
+    //   System.out.printf(" The value is: %d",a);
+    //   System.out.println();
+    //   }
+    // int i=0;
+    // while(i<=7)
+    // {
+    // System.out.println(i);
+    // i++;
+    // } 
+    // int i = 6;
+    // do{
+    //     System.out.println(i);
+    //     i--;    
+    // }
+    // while(i>0);
+    
+    }
+}
