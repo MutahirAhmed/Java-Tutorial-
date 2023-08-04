ea79b55744e219f68e0830a8a4035269b3deee49
MutahirAhmed <140383554+MutahirAhmed@users.noreply.github.com>
2023-08-04 10:51:36 +0500

    Add files via upload

---
+++ Arrays.java
@@ -0,0 +1,35 @@
+public class Arrays {
+    public static void main(String[] args) {
+        System.out.println("Arrays in Java");
+        //Arrays are Objects in java---> Object Class
+        // int [] arr = {1,2,3,4,5};
+        // System.out.println(arr[2]);
+        
+        // int [] arr = new int[5];
+        // arr[0] = 1;
+        // arr[1] = 11;
+        // arr[2] = 12;
+        // arr[3] = 13;
+        // arr[4] = 15;
+        // System.out.println(arr[5]);
+
+        // for(int i = 0; i < arr.length; i++)
+        // {
+        //     System.out.println("The Elements Are: " + arr[i]);
+        // }
+
+        // for(int i = arr.length - 1; i>=0; i--)
+        // {
+        //     System.out.println("The Elements Are: " + arr[i]);
+        // }
+
+        // String [] firstName = {"Farzeen","Ali","Mutahir","Zain","Ahmed"};
+        // for (int i = 0; i < firstName.length; i++) {
+        //     System.out.println("The Names Are: " + firstName[i]);
+        // }
+
+        // for (int i = firstName.length - 1 ; i>=0 ; i--) {
+        //     System.out.println("The Names Are: " + firstName[i]);
+        // }
+    }
+}
